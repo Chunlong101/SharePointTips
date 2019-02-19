97bd17819c338b8ef95963c19fa90c488a3f6a03
Chunlong <jolinner@126.com>
2019-02-19 15:51:35 +0800

    Create AddUserReadPermissionForAllSitesLists.ps1

---
+++ AddUserReadPermissionForAllSitesLists.ps1
@@ -0,0 +1,224 @@
+$tenantName = "xia053"
+
+$userName = "chunlonl@microsoft.com"
+
+$ErrorActionPreference = "Stop"
+
+#$cred = Get-Credential
+
+Function GetUniquePermissionsLists ($Web)  
+{  
+    $lists = @() 
+
+    try 
+    {
+        $listColl = Get-PnPList -Web $web -Includes HasUniqueRoleAssignments  
+       
+        foreach($list in $listColl)  
+        {    
+            if($list.HasUniqueRoleAssignments)  
+            {  
+                $lists += $list
+            }        
+        } 
+    }
+    catch 
+    {
+        Write-Host $_ -ForegroundColor Red 
+    }
+
+    return $lists
+}  
+  
+Function GetRootWeb()  
+{  
+    return Get-PnPWeb 
+}  
+  
+Function GetSubWebs()  
+{  
+    return Get-PnPSubWebs -Recurse 
+}  
+ 
+ Function CheckIfWebHasUniquePermission ($Web) 
+ {
+    $result = $false
+
+    try 
+    {
+        $result = Get-PnPProperty -ClientObject $Web -Property HasUniqueRoleAssignments
+    }
+    catch 
+    {
+        Write-Host $_ -ForegroundColor Red
+    }
+
+    return $result
+ } 
+
+ Function RemoveUserReadPermissionFromWeb ($Web, $userName) 
+ {
+    try 
+    {
+        "Removing read only permission for $($userName) on $($web.Url)" 
+                    
+        Set-PnPWebPermission -Web $Web.Id -User $userName -RemoveRole "Read" 
+    }
+    catch 
+    {
+        Write-Host $_ -ForegroundColor Red
+    }
+ } 
+
+ Function RemoveUserReadPermissionFromList ($Web, $List, $userName) 
+ {
+    try 
+    {
+        "Removing read only permission for $($userName) on $($web.Url)$($list.DefaultViewUrl)"
+
+        Set-PnPListPermission -Identity $list.Title -User $userName -RemoveRole 'Read' 
+    } 
+    catch 
+    {
+        Write-Host $_ -ForegroundColor Red        
+    } 
+ } 
+
+  Function AddUserReadPermissionFromWeb ($Web, $userName) 
+ {
+    try 
+    {
+        "Setting read only permission for $($userName) on $($web.Url)"
+    
+        Set-PnPWebPermission -Web $web.Id -User $userName -AddRole "Read"
+    }
+    catch 
+    {
+        Write-Host $_ -ForegroundColor Red
+    }
+ } 
+
+  Function AddUserReadPermissionFromList ($Web, $List, $userName) 
+ {
+    try 
+    {
+        "Setting read only permission for $($userName) on $($web.Url)$($list.DefaultViewUrl)"
+
+        Set-PnPListPermission -Identity $list.Title -User $userName -AddRole 'Read'
+    } 
+    catch 
+    {
+        Write-Host $_ -ForegroundColor Red        
+    } 
+ } 
+
+ Function RemoveUserReadPermissionFromAllSitesLists ($userName, $tenantName, $cred) 
+ {
+    Connect-PnPOnline -Url https://$tenantName.sharepoint.com -Credentials $cred
+
+    $sites = Get-PnPTenantSite
+
+    foreach ($site in $sites)
+    {
+        try 
+        {
+            Connect-PnPOnline -Url $site.Url -Credentials $cred
+        }
+        catch 
+        {
+            Write-Host $_ -ForegroundColor Red
+            continue
+        }
+
+        $webs = @()
+
+        $rootWeb = GetRootWeb
+
+        if ($rootWeb) 
+        {
+            $webs += $rootWeb
+        }
+
+        $subWebs = GetSubWebs
+
+        if ($subWebs) 
+        {
+            $webs += $subWebs
+        }
+
+        foreach ($web in $webs) 
+        {
+            if (CheckIfWebHasUniquePermission $web) 
+            {
+                RemoveUserReadPermissionFromWeb $web $userName
+            }
+
+            $lists = GetUniquePermissionsLists $web
+
+            foreach ($list in $lists) 
+            {
+                if (!$list.Hidden) 
+                {
+                    RemoveUserReadPermissionFromList $web $list $userName
+                }
+            }
+        }
+    }
+ }
+
+Function AddUserReadPermissionForAllSitesLists ($userName, $tenantName, $cred) 
+{
+    Connect-PnPOnline -Url https://$tenantName.sharepoint.com -Credentials $cred
+
+    $sites = Get-PnPTenantSite
+
+    foreach ($site in $sites)
+    {
+        try 
+        {
+            Connect-PnPOnline -Url $site.Url -Credentials $cred
+        }
+        catch 
+        {
+            Write-Host $_ -ForegroundColor Red
+            continue
+        }
+
+        $webs = @()
+
+        $rootWeb = GetRootWeb
+
+        if ($rootWeb) 
+        {
+            $webs += $rootWeb
+        }
+
+        $subWebs = GetSubWebs
+
+        if ($subWebs) 
+        {
+            $webs += $subWebs
+        }
+
+        foreach ($web in $webs) 
+        {
+            if (CheckIfWebHasUniquePermission $web) 
+            {
+                AddUserReadPermissionFromWeb $web $userName
+            }
+
+            $lists = GetUniquePermissionsLists $web
+
+            foreach ($list in $lists) 
+            {
+                if (!$list.Hidden) 
+                {
+                    AddUserReadPermissionFromList $web $list $userName
+                }
+            }
+        }
+    }
+}
+
+#RemoveUserReadPermissionFromAllSitesLists $userName $tenantName $cred
+AddUserReadPermissionForAllSitesLists $userName $tenantName $cred
