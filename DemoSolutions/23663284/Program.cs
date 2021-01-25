b9adc043a90e89d91b5c70cfed2caf3cdd7be605
Chunlong <jolinner@126.com>
2021-01-25 13:46:55 +0800

    .

--- DemoSolutions/23663284/Program.cs
+++ DemoSolutions/23663284/Program.cs
@@ -12,7 +12,7 @@ namespace _23663284
     {
         static void Main(string[] args)
         {
-            // RestSharp with graph api to download a file from sharepoint online 
+            // RestSharp with graph api to download a file from sharepoint online, download the file from response body 
             var client = new RestClient("https://graph.microsoft.com/v1.0/sites/siteid/drives/driveid/items/itemid/content");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
