9a822746beda82f6a4651265c0a93279e9c97d09
Chunlong <jolinner@126.com>
2021-01-29 16:08:37 +0800

    Update ChangeServiceAccountPasswords.ps1

--- ChangeServiceAccountPasswords.ps1
+++ ChangeServiceAccountPasswords.ps1
@@ -1,5 +1,5 @@
 # -----
-# This scirpt aims to automatically update the passwords of a service account used in sharepoint, iis as wellas windows services 
+# This scirpt aims to automatically update the passwords of a service account used in sharepoint, iis as well as windows services. Pls note, Microsoft doesn't provide production ready scripts, customers need to test/verify/extend this script by themselves. And this script is out of the support scope. 
 # -----
 
 Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force
