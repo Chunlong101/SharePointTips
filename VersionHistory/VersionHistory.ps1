ef12cbffbed3be89ba5328f7ce72e218a6869b7a
Chunlong <jolinner@126.com>
2022-04-06 14:36:52 +0800

    .

---
+++ VersionHistory/VersionHistory.ps1
@@ -0,0 +1,101 @@
+#
+# The following scripts are going to list all version history of a library (before "TargetVersionModifiedDateTime" 2018-03-23 18:20:20)
+#
+
+$ScriptsLocation = 'C:\Users\xxx\Desktop\Script' # The location of this ps1 file
+$Username = 'xxx@xxx.onmicrosoft.com' 
+$Url = 'https://xxx-my.sharepoint.com/personal/xxx_xxx_onmicrosoft_com' 
+$RootFolder = 'Documents'
+$TargetVersionModifiedDateTime = [System.Convert]::ToDateTime('2018-03-23 18:20:20') # The problematic date
+ 
+Import-Module ($ScriptsLocation + "\Microsoft.SharePoint.Client.dll")
+Import-Module ($ScriptsLocation + "\Microsoft.SharePoint.Client.Runtime.dll")
+Import-Module ($ScriptsLocation + "\SPOMod20170306.psm1")
+ 
+Connect-SPOCSOM -Username $Username -Url $Url
+ 
+$TargetList = Get-SPOList -IncludeAllProperties | ? { $_.Title -match $RootFolder } # This could return more than one list
+ 
+$TargetItems = Get-SPOListItems -ListTitle $TargetList.Title -Recursive -IncludeAllProperties $true
+ 
+$TargetUrl = @{ }
+ 
+foreach ($item in $TargetItems) {
+    $targetVersions = $null
+ 
+    try {
+        $targetVersions = Get-SPOListItemVersions -ItemID $item.ID -ListTitle $TargetList.Title -IncludeAllProperties $true
+    }
+    catch [System.Exception] {
+        "No versioning was found for item " + $item.GUID + " under " + $TargetList.Title
+        
+        continue
+    }
+ 
+    if (!$targetVersions) {
+        continue
+    }
+ 
+    $targetVersion = $null
+ 
+    foreach ($version in $targetVersions) {
+        $diff = $version.Created - $TargetVersionModifiedDateTime
+            
+        if ($diff.Days -le 0) {
+            # Before $TargetVersionModifiedDateTime
+            $targetVersion = $version
+            break
+        }
+    }
+ 
+    if ($TargetVersion) {
+        $TargetUrl.Add($Url + '/' + $targetVersion.Url, $targetVersion.Url.Split('/')[-1]) 
+    }
+}
+ 
+$TargetUrl | ogv
+
+#
+# The following scripts are going to list all version history of a library
+#
+
+$ScriptsLocation = 'C:\Users\xxx\Desktop\Script' # The location of this ps1 file
+$Username = 'xxx@xxx.onmicrosoft.com' 
+$Url = 'https://xxx.sharepoint.com' 
+$RootFolder = 'Documents'
+ 
+Import-Module ($ScriptsLocation + "\Microsoft.SharePoint.Client.dll")
+Import-Module ($ScriptsLocation + "\Microsoft.SharePoint.Client.Runtime.dll")
+Import-Module ($ScriptsLocation + "\SPOMod20170306.psm1")
+ 
+Connect-SPOCSOM -Username $Username -Url $Url
+ 
+$TargetList = Get-SPOList -IncludeAllProperties | ? { $_.Title -eq $RootFolder } 
+ 
+$TargetItems = Get-SPOListItems -ListTitle $TargetList.Title -Recursive -IncludeAllProperties $true
+ 
+$TargetUrl = @{ }
+ 
+foreach ($item in $TargetItems) {
+    $targetVersions = $null
+ 
+    try {
+        $targetVersions = Get-SPOListItemVersions -ItemID $item.ID -ListTitle $TargetList.Title -IncludeAllProperties $true
+    }
+    catch [System.Exception] {
+        "No versioning was found for item " + $item.GUID + " under " + $TargetList.Title
+        
+        continue
+    }
+ 
+    if (!$targetVersions -or $targetVersions -eq 'No versions available') {
+        continue
+    }
+    
+    $targetVersions | % {
+        $TargetUrl.Add($Url + '/' + $_.Url, $_.Url.Split('/')[-1]) 
+    }
+         
+}
+ 
+$TargetUrl | ogv
