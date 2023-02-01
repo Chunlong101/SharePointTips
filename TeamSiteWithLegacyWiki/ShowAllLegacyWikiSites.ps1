8903709c77275782f7d46e4f38c906922a9a9528
Chunlong <jolinner@126.com>
2023-02-01 18:39:02 +0800

    .

--- TeamSiteWithLegacyWiki/ShowAllLegacyWikiSites.ps1
+++ TeamSiteWithLegacyWiki/ShowAllLegacyWikiSites.ps1
@@ -3,7 +3,6 @@ Connect-PnPOnline -Url $tanentUrl -Interactive
 $groups = Get-PnPMicrosoft365Group -IncludeSiteUrl
 foreach ($group in $groups) {
     Connect-PnPOnline $group.SiteUrl -Interactive
-    $web = Get-PnPWeb
     $lists = Get-PnPList | ? {$_.Title -match "Wiki"}
     if ($lists -ne $null) 
     {
