147aa338dabb2a68e59680f8a125ef637b63842c
Chunlong <jolinner@126.com>
2023-04-15 16:20:11 +0800

    .

--- ConnectVpn.ps1
+++ ConnectVpn.ps1
@@ -6,11 +6,17 @@ if ($vpn.ConnectionStatus -eq 'Disconnected') {
     $retryDelay = 2
     do {
         $retry++
+        Write-Host "Connecting to $vpnName, attempt $retry of $maxRetries"
         $result = rasdial $vpnName
+        Write-Host $result
         $vpn = Get-VpnConnection -Name $vpnName
-        if ($result -match 'The connection attempt failed because the remote computer did not respond') {
-            Write-Host "The connection attempt failed because the remote computer did not respond, wait $retryDelay seconds and try again"
+        Write-Host "Connection status: $($vpn.connectionstatus)"
+        if ($vpn.connectionstatus -ne 'Connected') {
+            Write-Host "The connection attempt failed, wait $retryDelay seconds and try again"
             Start-Sleep -Seconds $retryDelay
+        }
+        else {
+            Write-Host "The connection attempt succeeded"
         }
     } while ($retry -lt $maxRetries -and $vpn.connectionstatus -ne 'Connected')
 }
