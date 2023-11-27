995c6057624a913792d0ed7e084317df1bb24857
Chunlong <jolinner@126.com>
2023-11-27 11:14:54 +0800

    Update RecrawlSite.ps1

--- RecrawlSite.ps1
+++ RecrawlSite.ps1
@@ -1,20 +1,20 @@
-.\Ctsinfo.ps1 https://teamtelstra.sharepoint.com/sites/ph2
+.\Ctsinfo.ps1 https://xxx.sharepoint.com/sites/xxx
 
-Prod-SSL (DPRODMGD102) PS C:\Users\chunlonl\VsCode\Repo\Test> Request-FarmSPSearchCommand -Farm $cio.farmid -GetCrawlInfo -DisplayUrl https://teamtelstra.sharepoint.com/sites/ph2
+Prod-SSL (xxx) PS C:\Users\chunlonl\VsCode\Repo\Test> Request-FarmSPSearchCommand -Farm $cio.farmid -GetCrawlInfo -DisplayUrl https://xxx.sharepoint.com/sites/xxx
 Checking result for job [Id=54355213]...
  
-Invoking command ShouldExecute on BOT20019-710 [07/15/2019 20:12:54]
+Invoking command ShouldExecute on xxx [07/15/2019 20:12:54]
 True
  
 Complete invoking ShouldExecute. ElapsedTime: [00:00:18.2499996]
  
-Invoking command Execute on BOT20019-710 [07/15/2019 20:13:13]
+Invoking command Execute on xxx [07/15/2019 20:13:13]
 Calling get crawl docs from url. IsLike on? False
 Checking the NoCrawl Status. To skip this step use -SkipIsNoCrawlEnabled
  
 Complete invoking Execute. ElapsedTime: [00:00:21.7812388]
  
-Invoking command IsComplete on BOT20019-710 [07/15/2019 20:13:36]
+Invoking command IsComplete on xxx [07/15/2019 20:13:36]
 IsComplete
 True
  
@@ -23,12 +23,12 @@ Complete invoking IsComplete. ElapsedTime: [00:00:08.6874893]
 Executing Request-FarmSPSearchCommand completed at [07/15/2019 20:13:47] (Total Execution time: 00:02:31.6713076)
  
 DocId                     : 7928
-DatabaseName              : DedicatedContent_869643
-SiteId                    : 41118633-293c-4eb4-abc0-936b309cbb67
+DatabaseName              : xxx
+SiteId                    : xxx
 Protocol                  :
-DisplayUrl                : https://teamtelstra.sharepoint.com/sites/<PII:H1(YXKcAT13d2EI3IcZECB0Gy7UWC2liGOZlVGjaB6nWfs=)>
+DisplayUrl                : https://xxx.sharepoint.com/sites/<PII:H1(YXKcAT13d2EI3IcZECB0Gy7UWC2liGOZlVGjaB6nWfs=)>
 ParentDocID               : 1
-ObjectId                  : {41118633-293c-4eb4-abc0-936b309cbb67}_{41118633-293c-4eb4-abc0-936b309cbb67}
+ObjectId                  : {xxx}_{xxx}
 AccessData                : Site
 NoIndex                   : True
 DocumentFlags             :
@@ -46,16 +46,16 @@ ErrorCountSecurityOnly    : 0
 CorrelationID             :
 BSN                       :
 ScsDocIndexType           : sp-site
-ScsDocIndexName           : 41118633-293c-4eb4-abc0-936b309cbb67
+ScsDocIndexName           : xxx
 ScsClientId               : 3bfb2eca5d726e0826e7ae5652b5bf2d822d10eb9f26d725c6e514843d329dab
 
 DocId                     : 21546
-DatabaseName              : DedicatedContent_869643
-SiteId                    : 41118633-293c-4eb4-abc0-936b309cbb67
+DatabaseName              : xxx
+SiteId                    : xxx
 Protocol                  :
-DisplayUrl                : https://teamtelstra.sharepoint.com/sites/<PII:H1(YXKcAT13d2EI3IcZECB0Gy7UWC2liGOZlVGjaB6nWfs=)>
+DisplayUrl                : https://xxx.sharepoint.com/sites/<PII:H1(YXKcAT13d2EI3IcZECB0Gy7UWC2liGOZlVGjaB6nWfs=)>
 ParentDocID               : 7928
-ObjectId                  : {41118633-293c-4eb4-abc0-936b309cbb67}_{fbb000d2-532f-4a52-bee7-ec9df24c5de2}
+ObjectId                  : {xxx}_{fbb000d2-532f-4a52-bee7-ec9df24c5de2}
 AccessData                : Web
 NoIndex                   : False
 DocumentFlags             :
@@ -73,10 +73,10 @@ ErrorCountSecurityOnly    : 0
 CorrelationID             :
 BSN                       :
 ScsDocIndexType           : sp-site
-ScsDocIndexName           : 41118633-293c-4eb4-abc0-936b309cbb67
+ScsDocIndexName           : xxx
 ScsClientId               : eb615c2f396c818714d03bbd299fad3c1aebcbec7bc744ac1021a9a47295f768
 
-Prod-SSL (DPRODMGD102) PS C:\Users\chunlonl\VsCode\Repo\Test> Request-FarmSPSearchCommand -Farm $cio.farmid -MarkDocumentForRecrawl –docid 7928 -DBName DedicatedContent_869643
+Prod-SSL (xxx) PS C:\Users\chunlonl\VsCode\Repo\Test> Request-FarmSPSearchCommand -Farm $cio.farmid -MarkDocumentForRecrawl –docid 7928 -DBName xxx
 Checking result for job [Id=54363276]...
  
  
