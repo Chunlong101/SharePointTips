0a85f76850a5309d230e37fec7461464c586c9c8
Chunlong <jolinner@126.com>
2024-05-12 17:52:10 +0800

    .

---
+++ IntermittentIssueDetector/IntermittentIssueDetector/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace IntermittentIssueDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.urlTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.frequencyTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.SuccessTagTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.goButton = new MetroFramework.Controls.MetroButton();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.metroContextMenu1 = new MetroFramework.Controls.MetroContextMenu(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runInBackgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openLogButton = new MetroFramework.Controls.MetroButton();
+            this.stopButton = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.metroContextMenu1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 999999999;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // urlTextBox
+            // 
+            // 
+            // 
+            // 
+            this.urlTextBox.CustomButton.Image = null;
+            this.urlTextBox.CustomButton.Location = new System.Drawing.Point(874, 2);
+            this.urlTextBox.CustomButton.Name = "";
+            this.urlTextBox.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.urlTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.urlTextBox.CustomButton.TabIndex = 1;
+            this.urlTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.urlTextBox.CustomButton.UseSelectable = true;
+            this.urlTextBox.CustomButton.Visible = false;
+            this.urlTextBox.Lines = new string[] {
+        "Pls input your web url here, for example: https://github.com/Chunlong101"};
+            this.urlTextBox.Location = new System.Drawing.Point(23, 113);
+            this.urlTextBox.MaxLength = 32767;
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.PasswordChar = '\0';
+            this.urlTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.urlTextBox.SelectedText = "";
+            this.urlTextBox.SelectionLength = 0;
+            this.urlTextBox.SelectionStart = 0;
+            this.urlTextBox.ShortcutsEnabled = true;
+            this.urlTextBox.Size = new System.Drawing.Size(916, 44);
+            this.urlTextBox.TabIndex = 2;
+            this.urlTextBox.Text = "Pls input your web url here, for example: https://github.com/Chunlong101";
+            this.urlTextBox.UseSelectable = true;
+            this.urlTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.urlTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // frequencyTextBox
+            // 
+            // 
+            // 
+            // 
+            this.frequencyTextBox.CustomButton.Image = null;
+            this.frequencyTextBox.CustomButton.Location = new System.Drawing.Point(54, 2);
+            this.frequencyTextBox.CustomButton.Name = "";
+            this.frequencyTextBox.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.frequencyTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.frequencyTextBox.CustomButton.TabIndex = 1;
+            this.frequencyTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.frequencyTextBox.CustomButton.UseSelectable = true;
+            this.frequencyTextBox.CustomButton.Visible = false;
+            this.frequencyTextBox.Lines = new string[] {
+        "Frequency"};
+            this.frequencyTextBox.Location = new System.Drawing.Point(1287, 114);
+            this.frequencyTextBox.MaxLength = 32767;
+            this.frequencyTextBox.Name = "frequencyTextBox";
+            this.frequencyTextBox.PasswordChar = '\0';
+            this.frequencyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.frequencyTextBox.SelectedText = "";
+            this.frequencyTextBox.SelectionLength = 0;
+            this.frequencyTextBox.SelectionStart = 0;
+            this.frequencyTextBox.ShortcutsEnabled = true;
+            this.frequencyTextBox.Size = new System.Drawing.Size(96, 44);
+            this.frequencyTextBox.TabIndex = 4;
+            this.frequencyTextBox.Text = "Frequency";
+            this.frequencyTextBox.UseSelectable = true;
+            this.frequencyTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.frequencyTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // SuccessTagTextBox
+            // 
+            // 
+            // 
+            // 
+            this.SuccessTagTextBox.CustomButton.Image = null;
+            this.SuccessTagTextBox.CustomButton.Location = new System.Drawing.Point(294, 2);
+            this.SuccessTagTextBox.CustomButton.Name = "";
+            this.SuccessTagTextBox.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.SuccessTagTextBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.SuccessTagTextBox.CustomButton.TabIndex = 1;
+            this.SuccessTagTextBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.SuccessTagTextBox.CustomButton.UseSelectable = true;
+            this.SuccessTagTextBox.CustomButton.Visible = false;
+            this.SuccessTagTextBox.Lines = new string[] {
+        "SuccessTag"};
+            this.SuccessTagTextBox.Location = new System.Drawing.Point(945, 114);
+            this.SuccessTagTextBox.MaxLength = 32767;
+            this.SuccessTagTextBox.Name = "SuccessTagTextBox";
+            this.SuccessTagTextBox.PasswordChar = '\0';
+            this.SuccessTagTextBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SuccessTagTextBox.SelectedText = "";
+            this.SuccessTagTextBox.SelectionLength = 0;
+            this.SuccessTagTextBox.SelectionStart = 0;
+            this.SuccessTagTextBox.ShortcutsEnabled = true;
+            this.SuccessTagTextBox.Size = new System.Drawing.Size(336, 44);
+            this.SuccessTagTextBox.TabIndex = 6;
+            this.SuccessTagTextBox.Text = "SuccessTag";
+            this.SuccessTagTextBox.UseSelectable = true;
+            this.SuccessTagTextBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.SuccessTagTextBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(1389, 113);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(114, 43);
+            this.goButton.TabIndex = 7;
+            this.goButton.Text = "Go";
+            this.goButton.UseSelectable = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(23, 163);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1765, 748);
+            this.webView21.TabIndex = 9;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Intermittent Issue Detector";
+            this.notifyIcon1.ContextMenuStrip = this.metroContextMenu1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Intermittent Issue Detector";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // metroContextMenu1
+            // 
+            this.metroContextMenu1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.metroContextMenu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.runInBackgroundToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.metroContextMenu1.Name = "metroContextMenu1";
+            this.metroContextMenu1.ShowItemToolTips = false;
+            this.metroContextMenu1.Size = new System.Drawing.Size(266, 100);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(265, 32);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // runInBackgroundToolStripMenuItem
+            // 
+            this.runInBackgroundToolStripMenuItem.Name = "runInBackgroundToolStripMenuItem";
+            this.runInBackgroundToolStripMenuItem.Size = new System.Drawing.Size(265, 32);
+            this.runInBackgroundToolStripMenuItem.Text = "Run in the background";
+            this.runInBackgroundToolStripMenuItem.Click += new System.EventHandler(this.runInBackgroundToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(265, 32);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openLogButton
+            // 
+            this.openLogButton.Location = new System.Drawing.Point(1629, 113);
+            this.openLogButton.Name = "openLogButton";
+            this.openLogButton.Size = new System.Drawing.Size(159, 43);
+            this.openLogButton.TabIndex = 10;
+            this.openLogButton.Text = "Open Log File";
+            this.openLogButton.UseSelectable = true;
+            this.openLogButton.Click += new System.EventHandler(this.openLogButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(1509, 113);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(114, 43);
+            this.stopButton.TabIndex = 11;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseSelectable = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1811, 934);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.openLogButton);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.SuccessTagTextBox);
+            this.Controls.Add(this.frequencyTextBox);
+            this.Controls.Add(this.urlTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Intermittent Issue Dectector";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.metroContextMenu1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroTextBox urlTextBox;
+        private MetroFramework.Controls.MetroTextBox frequencyTextBox;
+        private MetroFramework.Controls.MetroTextBox SuccessTagTextBox;
+        private MetroFramework.Controls.MetroButton goButton;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private MetroFramework.Controls.MetroContextMenu metroContextMenu1;
+        private System.Windows.Forms.ToolStripMenuItem runInBackgroundToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private MetroFramework.Controls.MetroButton openLogButton;
+        private MetroFramework.Controls.MetroButton stopButton;
+    }
+}
+
---
+++ IntermittentIssueDetector/IntermittentIssueDetector/Form1.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.Web.WebView2.Core;
+using NLog;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace IntermittentIssueDetector
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private Stopwatch stopwatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeLogger();
+            // Don''t allow maximize the form
+            MaximizeBox = false;
+        }
+
+        private void InitializeLogger()
+        {
+            // Config NLog, log to file
+            var config = new NLog.Config.LoggingConfiguration();
+            var logfile = new NLog.Targets.FileTarget("csvFile")
+            {
+                FileName = "log.csv",
+                Layout = "${longdate},${level},${message}"
+            };
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+            LogManager.Configuration = config;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Logger.Debug("Entering Form1_Load");
+
+                InitializeBrowser();
+                Logger.Debug("Browser initialized");
+
+                frequencyTextBox.Text = "99999";
+                Logger.Debug("Frequency set to 99999 seconds");
+
+                Logger.Debug("Leaving Form1_Load");
+            }
+            catch (Exception ex)
+            {
+                Logger.Fatal("Form1_Load failed");
+                Logger.Fatal(ex.Message);
+                Logger.Fatal(ex.StackTrace);
+            }
+        }
+
+        async void InitializeBrowser()
+        {
+            // Create WebView2 environment to have SSO enabled for system primary account
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions()
+            {
+                AllowSingleSignOnUsingOSPrimaryAccount = true
+            };
+            Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--enable-features=msSingleSignOnOSForPrimaryAccountIsShared");
+            CoreWebView2Environment environment = CoreWebView2Environment.CreateAsync(null, null, options).Result;
+            await webView21.EnsureCoreWebView2Async(environment);
+            webView21.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+            webView21.CoreWebView2.WebMessageReceived += webView21_WebMessageReceived;
+        }
+
+        private void CoreWebView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            try
+            {
+                Logger.Debug("Entering CoreWebView2_NavigationCompleted");
+
+                stopwatch.Stop();
+                long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+                Logger.Info($"Page loaded in {elapsedMilliseconds} milliseconds, url: {webView21.CoreWebView2.Source}");
+
+                //// Execute JavaScript to get the number of elements with the class name, and send the number to native with postMessage, how many elements do we have with the same class name
+                //webView21.CoreWebView2.ExecuteScriptAsync("" +
+                //    "var tag = document.getElementsByClassName('xxx');".Replace("xxx", SuccessTagTextBox.Text) +
+                //    "window.chrome.webview.postMessage(tag.length.toString());");
+
+                // Execute JavaScript to find the element with the text, and send the element to native with postMessage, as long as the page contains the text
+                webView21.CoreWebView2.ExecuteScriptAsync("" +
+                    "const targetText = 'xxx';".Replace("xxx", SuccessTagTextBox.Text) +
+                    "const element = Array.from(document.querySelectorAll('*')).find(el => el.textContent.includes(targetText));" +
+                    "if (element) { window.chrome.webview.postMessage(element.toString()); } else { window.chrome.webview.postMessage('Element not found'); }" +
+                    "");
+
+                Logger.Debug("Leaving CoreWebView2_NavigationCompleted");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("CoreWebView2_NavigationCompleted failed");
+                Logger.Error($"{ex.Message}");
+                Logger.Error($"{ex.StackTrace}");
+            }
+        }
+
+        private void webView21_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            try
+            {
+                Logger.Debug("Entering webView21_WebMessageReceived");
+
+                string message = e.TryGetWebMessageAsString();
+
+                //// Check if the message is a number, how many elements do we have with the same class name
+                //if (int.TryParse(message, out int result))
+                //{
+                //    // If the number is greater than 0, the page is loaded
+                //    if (result > 0)
+                //    {
+                //        Logger.Info("Page loaded successfully");
+                //    }
+                //    else
+                //    {
+                //        Logger.Error("Page failed to load");
+                //    }
+                //}
+                //else
+                //{
+                //    Logger.Error("Message received is not a number");
+                //}
+
+                // Check if the message is not empty and not "Element not found", as long as the page contains the text
+                if (!string.IsNullOrEmpty(message) && message != "Element not found")
+                {
+                    Logger.Info($"Page loaded successfully, url: {webView21.CoreWebView2.Source}, tag: {SuccessTagTextBox.Text}");
+                }
+                else
+                {
+                    Logger.Error($"Page failed to load, url: {webView21.CoreWebView2.Source}, tag: {SuccessTagTextBox.Text}");
+                }
+
+                Logger.Debug("Leaving webView21_WebMessageReceived");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("webView21_WebMessageReceived failed");
+                Logger.Error($"{ex.Message}");
+                Logger.Error($"{ex.StackTrace}");
+            }
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Logger.Debug("Entering goButton_Click");
+
+                stopwatch = Stopwatch.StartNew();
+                webView21.CoreWebView2.Navigate(urlTextBox.Text);
+                // Set the timer interval to frequencyTextBox
+                timer1.Interval = Convert.ToInt32(frequencyTextBox.Text) * 1000;
+                timer1.Start();
+                Logger.Debug("Timer started");
+
+                Logger.Debug("Leaving goButton_Click");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("goButton_Click failed");
+                Logger.Error($"{ex.Message}");
+                Logger.Error($"{ex.StackTrace}");
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+            Logger.Debug("Timer stopped");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Logger.Debug("Entering timer1_Tick");
+
+                stopwatch = Stopwatch.StartNew();
+                webView21.CoreWebView2.Navigate(urlTextBox.Text);
+
+                Logger.Debug("Leaving timer1_Tick");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("timer1_Tick failed");
+                Logger.Error($"{ex.Message}");
+                Logger.Error($"{ex.StackTrace}");
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // If the form is minimized, restore it to normal state, otherwise minimize it
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // If the form is minimized, hide it and show the notify icon
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                metroContextMenu1.Show(Cursor.Position);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Restore the form to normal state
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void runInBackgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Minimize the form if it is not minimized
+            if (WindowState != FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openLogButton_Click(object sender, EventArgs e)
+        {
+            // Open the log file with the default text editor
+            Process.Start("log.csv");
+        }
+    }
+}
